6620859e0c03d51f16870ec6c1e08c225c03c033
Diana <crabbit210@gmail.com>
2019-04-10 00:32:10 -0700

    SYN-24

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -187,7 +187,7 @@ class Navbar extends React.Component {
                                   <div className="right-content">
                                     <h5>Try Oasis Now</h5>
                                     <h6>Discover all of your enterprise network and device data - simply and quickly. Then view that data in a logical and digestible UI. This is the first step to having massive visibility into all systems and is critical to decision making, security, and capacity management.</h6>
-                                    <Button color="danger">Learn More</Button>
+                                    <a color="danger" className="learnmore" href="https://www.hubspot.com/">Learn More</a>
                                   </div>
                                 </div>
                               </div>
