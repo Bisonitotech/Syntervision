4b4bb70f81aed9e86d55349ea1be2b9dbcc6c304
Diana <crabbit210@gmail.com>
2019-05-09 06:46:23 -0700

    nextjs project

---
+++ components/Includes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import stylesheet from 'styles/css/main.scss';
+
+class Includes extends React.Component {
+
+
+
+  render() {
+
+  	return (
+        <div>
+          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <script src='static/js/jquery.min.js'></script>
+          <script src='static/js/bootstrap.bundle.min.js'></script>
+          <script src='static/js/jquery.easing.1.3.min.js'></script>
+          <script src='static/js/scrollspy.min.js'></script>
+          <script src='static/js/jquery.sticky.js'></script>
+          <script src='static/js/waves.min.js'></script>
+          <script src='static/js/jquery.magnific-popup.min.js'></script>
+          <script src='static/js/switcher.js'></script>
+          <script src='static/js/app.js'></script>
+        </div>
+  	);
+  }
+}
+
+export default Includes;
