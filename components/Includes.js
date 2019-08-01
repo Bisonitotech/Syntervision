212f77ffc245f5eacac765f3fd960b9e7bce5fe6
Diana <crabbit210@gmail.com>
2019-08-01 22:17:38 +0800

    SYN-66

--- components/Includes.js
+++ components/Includes.js
@@ -5,13 +5,27 @@ import stylesheet from 'styles/css/main.scss';
 class Includes extends React.Component {
 
 
+  setGoogleTags() {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag()
 
+          {dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-128652726-1');
+
+        `
+      };
+    }
   render() {
 
   	return (
         <div>
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <script dangerouslySetInnerHTML={this.setGoogleTags()} />
           <script src='/static/js/jquery.min.js'></script>
           <script src='/static/js/bootstrap.bundle.min.js'></script>
           <script src='/static/js/jquery.easing.1.3.min.js'></script>
@@ -21,6 +35,10 @@ class Includes extends React.Component {
           <script src='/static/js/jquery.magnific-popup.min.js'></script>
           <script src='/static/js/switcher.js'></script>
           <script src='/static/js/app.js'></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128652726-1"></script>
+          
+
+
         </div>
   	);
   }
